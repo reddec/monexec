dc6ad49b5dc22e6a8405035864c7b66db4e27bc5
Alexander Baryshnikov <dev@baryshnikov.net>
2019-01-11 20:34:41 +0800

    add release script

---
+++ release.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+version=$(git describe | cut -c1-7)
+snapcraft list-revisions fluent-amqp | grep $version  | awk '{print $1}' | xargs -n 1 -i snapcraft release fluent-amqp '{}' beta,candidate,stable
+goreleaser --rm-dist
