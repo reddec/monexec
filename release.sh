3fda9980b25891928cb6b0900a89f27533fd459f
Alexander Baryshnikov <dev@baryshnikov.net>
2019-01-11 20:38:52 +0800

    typos fix in snap build

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 version=$(git describe --tags | cut -c1-7)
-snapcraft list-revisions fluent-amqp | grep $version  | awk '{print $1}' | xargs -n 1 -i snapcraft release fluent-amqp '{}' beta,candidate,stable
+snapcraft list-revisions monexec | grep $version  | awk '{print $1}' | xargs -n 1 -i snapcraft release monexec '{}' beta,candidate,stable
 goreleaser --rm-dist
